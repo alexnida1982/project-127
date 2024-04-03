3a4afd5341df7aa6e5c11ab3d1c39b55b55b773d
alexnida1982 <79065467+alexnida1982@users.noreply.github.com>
2024-04-02 20:11:13 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+music.mp3 = 0;
+music2.mp3 = 0;
+
+
+function preload()
+{
+    loadSound()
+    {
+        music.mp3
+        music2.mp3
+    }
+}
+
+function setup()
+{
+    image()
+    {
+        
+    }
+}
